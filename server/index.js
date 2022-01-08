7239f302d2590cfef380b3d2a6ea839c5aa04140
christygomez <christygomez@gmail.com>
2022-01-08 14:08:11 -0500

    server connected client working

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const UserModel = require("./models/Users");
+const PORT = process.env.PORT || 3001;
 
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
+
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://user123:Password123Tech@cluster0.j7fql.mongodb.net/merntutorial?retryWrites=true&w=majority"
-);
-
-app.get("/getUsers", (req, res) => {
+app.get('/getUsers', (req, res) => {
   UserModel.find({}, (err, result) => {
     if (err) {
       res.json(err);
@@ -22,7 +22,7 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
-app.post("/createUser", async (req, res) => {
+app.post('/createUser', async (req, res) => {
   const user = req.body;
   const newUser = new UserModel(user);
   await newUser.save();
@@ -30,6 +30,14 @@ app.post("/createUser", async (req, res) => {
   res.json(user);
 });
 
-app.listen(3001, () => {
-  console.log("SERVER RUNS PERFECTLY!");
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
