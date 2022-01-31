c5f5815c9c6b15b42bdbb771c627ac2a29c8bb8a
christygomez <christygomez@gmail.com>
2022-01-30 20:52:57 -0500

    postman working and front and back connected but started loading slow

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,196 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import { useState, useEffect } from 'react';
-import Axios from 'axios';
-import UsersTable from './components/UsersTable';
+
+// import ListingForm from './components/ListingForm';
 
 function App() {
-  const [listOfUsers, setListOfUsers] = useState([]);
+  const [listOfListings, setListOfListings] = useState([]);
   const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
-  const [username, setUsername] = useState('');
+  const [summary, setSummary] = useState('');
+  const [property_type, setProperty_type] = useState('');
+  const [market, setMarket] = useState('');
+  const [country, setCountry] = useState('');
+  const [bedrooms, setBedrooms] = useState('');
+  const [bathrooms, setBathrooms] = useState('');
+  const [price, setPrice] = useState('');
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/getUsers').then((response) => {
-      setListOfUsers(response.data);
-    });
+    getData();
   }, []);
 
-  const createUser = () => {
-    Axios.post('http://localhost:3001/createUser', {
-      name,
-      age,
-      username,
-    }).then((response) => {
-      setListOfUsers([
-        ...listOfUsers,
-        {
-          name,
-          age,
-          username,
-        },
-      ]);
+  const getData = async () => {
+    await axios.get('http://localhost:3001/getListings').then((response) => {
+      setListOfListings(response.data);
     });
   };
 
-  const deleteUser = (id) => {
-    const updatedUser = [...listOfUsers].filter((user) => user._id != id);
-    console.log('DELETE USER');
+  const createListing = () => {
+    axios
+      .post('http://localhost:3001/createListing', {
+        name,
+        summary,
+        property_type,
+        address: { market, country },
+        bedrooms,
+        bathrooms,
+        price,
+      })
+      .then((response) => {
+        setListOfListings([
+          ...listOfListings,
+          {
+            name,
+            summary,
+            property_type,
+            market,
+            country,
+            bedrooms,
+            bathrooms,
+            price,
+          },
+        ]);
+      });
+    clear();
   };
 
-  const editUser = (id) => {
-    const editedUser = [...listOfUsers].filter((user) => user._id != id);
-    console.log('EDIT USER');
+  const deleteListing = (id) => {
+    // console.log('new id past in delet function: ', id);
+    // const listing = [...listOfListings].filter(
+    //   (listing) => listing.new_id != id
+    // );
+    setListOfListings((listings) => {
+      [...listOfListings].filter((listing) => listing.new_id !== listing.id);
+    });
+    // console.log('ID IS: ', id);
+    // const result = axios
+    //   .get(`http://localhost:3001/listings/${id}`)
+    //   .then((response) => {
+    //     return response.data;
+    //   });
+    // console.log('RESULT VARIABLE', result);
+    // axios.delete(`https://localhost/3001/listings/${id}`).catch((error) => {
+    //   console.error('There was an error!', error);
+    // });
+    // await axios
+    //   .delete(`https://localhost/3001/listings/${new_id}`)
+    //   .catch((error) => {
+    //     console.log('error: ', error);
+    //   });
+    // await axios
+    //   .delete(`https://localhost/3001/listings/${new_id}`)
+    //   .catch((err) => console.log(err));
+    // const posts = this.state.posts.filter(item => item.id !== id);
+    // this.setState({ posts });
+    return console.log(id);
+  };
+
+  const editListing = (id) => {
+    // const editedUser = [...listOfUsers].filter((user) => user._id != id);
+    // console.log('EDIT USER');
+    // console.log('id is: ', id);
+  };
+
+  const clear = () => {
+    setName('');
+    setSummary('');
+    setProperty_type('');
+    setMarket('');
+    setCountry('');
+    setBedrooms('');
+    setBathrooms('');
+    setPrice('');
   };
 
   return (
     <div className='App'>
-      {/* <UsersTable></UsersTable> */}
-      <div className='usersDisplay'>
-        {listOfUsers.map((user) => {
-          return (
-            <div key={user._id}>
-              <h1>
-                Name: {user.name}, Age: {user.age}, Username: {user.username}
-                <button onClick={() => editUser(user._id)}>Edit</button>
-                <button onClick={() => deleteUser(user._id)}>Delete</button>
-              </h1>
-            </div>
-          );
-        })}
-      </div>
-      <div>
+      <form className='AddListingForm'>
         <input
           type='text'
-          placeholder='Name...'
+          placeholder='Listing Name'
           onChange={(event) => {
             setName(event.target.value);
           }}
-        />
-        <input
-          type='number'
-          placeholder='Age...'
-          onChange={(event) => {
-            setAge(event.target.value);
-          }}
-        />
+        ></input>
         <input
           type='text'
-          placeholder='Username...'
+          placeholder='Summary'
           onChange={(event) => {
-            setUsername(event.target.value);
+            setSummary(event.target.value);
           }}
-        />
-        <button onClick={createUser}> Create User </button>
+        ></input>
+        <input
+          type='text'
+          placeholder='Property Type'
+          onChange={(event) => {
+            setProperty_type(event.target.value);
+          }}
+        ></input>
+        <input
+          type='text'
+          placeholder='City'
+          onChange={(event) => {
+            setMarket(event.target.value);
+          }}
+        ></input>
+        <input
+          type='text'
+          placeholder='Country'
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        ></input>
+        <input
+          type='number'
+          placeholder='Bedrooms'
+          onChange={(event) => {
+            setBedrooms(event.target.value);
+          }}
+        ></input>
+        <input
+          type='number'
+          placeholder='Bathrooms'
+          onChange={(event) => {
+            setBathrooms(event.target.value);
+          }}
+        ></input>
+        <input
+          type='number'
+          placeholder='Price'
+          onChange={(event) => {
+            setPrice(event.target.value);
+          }}
+        ></input>
+
+        <button onClick={createListing}> Add Listing </button>
+      </form>
+      <div className='ListingsDisplay'>
+        {listOfListings.slice(-5).map((listing) => {
+          return (
+            <div key={listing._id} id={listing.new_id} value={listing.new_id}>
+              <h1>Listing: {listing.name}</h1>
+
+              <h2>Summary: {listing.summary}</h2>
+
+              <h2>
+                City: {listing.address.market}, Country:{' '}
+                {listing.address.country}
+              </h2>
+              <h2>
+                Property Type: {listing.property_type} Bedrooms:{' '}
+                {listing.bedrooms}, Bathrooms: {listing.bathrooms}, Price:{' '}
+                {listing.price}
+              </h2>
+              <h3>
+                Old ID:{listing._id} New ID: {listing.new_id}
+              </h3>
+              <button onClick={() => editListing(listing._id)}>Edit</button>
+              <button onClick={() => deleteListing(listing.new_id)}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
